a2875ab634ec61d6a3fe070378621832dab732cf
truong878664 <trandao056@gmail.com>
2022-10-05 19:32:08 +0700

    debug language

--- render.js
+++ render.js
@@ -1,29 +1,27 @@
-
-const start =  () => {
-    renderHeader()
-    renderFooter()
-}
+const start = () => {
+  renderHeader();
+  renderFooter();
+};
 
 function renderHeader() {
-    const header = document.querySelector('#header')
-    fetch('/layout/header.html')
-    .then(res => res.text())
-    .then(data => {header.innerHTML=data
-        const parser = new DOMParser()
-        const doc = parser.parseFromString(data,'text/html')
-        eval(doc.querySelector('script').textContent)})
+  const header = document.querySelector("#header");
+  fetch("/layout/header.html")
+    .then((res) => res.text())
+    .then((data) => {
+      header.innerHTML = data;
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(data, "text/html");
+      eval(doc.querySelector("script").textContent);
+    });
 }
 
-function renderFooter () {
-    const footer = document.querySelector('#footer')
-    fetch('/layout/footer.html')
-    .then(res => res.text())
-    .then(data => {
-        footer.innerHTML=data
-})
+function renderFooter() {
+  const footer = document.querySelector("#footer");
+  fetch("/layout/footer.html")
+    .then((res) => res.text())
+    .then((data) => {
+      footer.innerHTML = data;
+    });
 }
 
-
-start()
-
-
+start();
